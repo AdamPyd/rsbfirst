e0d24659ce94230018ef4c5ea2b183065a61f7c3
AdamPyd <285307542@qq.com>
2020-05-04 22:04:07 +0800

    [init-project]-init-spring boot

---
+++ src/test/java/com/adam/rsbfiirst/RsbfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adam.rsbfiirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsbfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
